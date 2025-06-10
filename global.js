02cc0f672e8da54a9bcd37ca811c745819d56b88
momato-0 <haiiro.cvs@gmail.com>
2025-06-10 22:26:43 +0900

    20250610-23

--- global.js
+++ global.js
@@ -1,4 +1,4 @@
-fetch('/nav.html')
+fetch('/momato-0/play-with-the-monster/nav.html')
     .then(res => res.text())
     .then(html => {
         document.getElementById('global-nav').innerHTML = html;
