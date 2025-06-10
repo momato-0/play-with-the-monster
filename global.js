2e46edb89b6cc6aaf4456be42d93ce679dfc441b
momato-0 <haiiro.cvs@gmail.com>
2025-06-10 22:24:03 +0900

    20250610-23

--- global.js
+++ global.js
@@ -1,6 +1,5 @@
-fetch('nav.html')
+fetch('/nav.html')
     .then(res => res.text())
     .then(html => {
-        console.log('loaded nav:', html); // ← ここで中身確認
         document.getElementById('global-nav').innerHTML = html;
     });
